d55d226b57bb8caf2d529a4b2dd49ff6546270e4
Derek Zhao <dz33@students.cs.ubc.ca>
2025-02-15 11:59:28 +0900

    added implementation for Movie

--- src/main/model/Movie.java
+++ src/main/model/Movie.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.*;
 
 // Represents a movie. Each movie's status is: "Watched", "Currently Watching", or "To-watch".
 // Each movie has a genre, a total watch time in minutes, notes, and a review. 
@@ -16,71 +16,83 @@ public class Movie {
     // REQUIRES: length of name and genre > 0 AND status must be one of: "Watched", "Currently Watching", or "To-watch"
     // EFFECTS: creates a MediaItem with status, name, genre, empty notes, no rating, and zero minutes watchTime
     public Movie(String status, String name, String genre) {
-        // TODO
+        this.status = status;
+        this.name = name;
+        this.genre = genre;
+        this.notes = new ArrayList<>();
+        this.rating = 0;
+        this.watchTime = 0;
     }
 
     // REQUIRES: length of newNote > 0
     // MODIFIES: this
     // EFFECTS: adds a new note to the list of notes
     public void addNote(String newNote) {
-        // TODO
+        this.notes.add(newNote);
     }
 
     // REQUIRES: noteNum >= 1 AND noteNum <= notes.size() AND notes.size() > 0
     // MODIFIES: this
     // EFFECTS: removes the note that matches the given number
     public void removeNote(int noteNum) {
-        // TODO
+        this.notes.remove(noteNum - 1);
     }
 
     // REQUIRES: toAdd > 0
     // MODIFIES: this
     // EFFECTS: adds more watch time to the current watch time
     public void addWatchTime(int toAdd) {
-        // TODO
+        this.watchTime += toAdd;
     }
 
     public String getStatus() {
-        return ""; // stub
+        return this.status;
     }
     
     public String getName() {
-        return ""; // stub
+        return this.name;
     }
 
     public String getGenre() {
-        return ""; // stub
+        return this.genre;
     }
 
     public String getNotes() {
-        return ""; // stub 
+        String result = "";
+        int count = 
+
+        for (String s : this.notes) {
+            result = result + s + "\n";
+        }
+
+        return result;
     }
 
     public int getRating() {
-        return -1; // stub
+        return this.rating; // stub
     }
 
     public int getWatchTime() {
-        return -1; // stub
+        return this.watchTime; // stub
     }
 
     // REQUIRES: status must be one of: "Watched", "Currently Watching", or "To-watch"
     public void setStatus(String status) {
-        // TODO
+        this.status = status;
     }
 
     // REQUIRES: length of name > 0
     public void setName(String name) {
-        // TODO
+        this.name = name;
     }
 
     // REQUIRES: length of genre > 0
     public void setGenre(String genre) {
-        // TODO
+        this.genre = genre;
     }
 
     // REQUIRES: rating >= 0 AND rating <= 5
     public void setRating(int rating) {
-        // TODO
+        this.rating = rating;
     }
 }
