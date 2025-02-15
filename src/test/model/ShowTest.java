b19b2cdcc675a879a586b0518f520bac2201b3a8
Derek Zhao <dz33@students.cs.ubc.ca>
2025-02-15 10:09:39 +0900

    added tests for Show class

---
+++ src/test/model/ShowTest.java
@@ -0,0 +1,74 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowTest {
+
+    private Show testShow;
+
+    @BeforeEach
+    public void setup() {
+        testShow = new Show("Watched", "test", "genre");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Watched", testShow.getStatus());
+        assertEquals("test", testShow.getName());
+        assertEquals("genre", testShow.getGenre());
+        assertEquals(List.of(), testShow.getNotes());
+        assertEquals(0, testShow.getRating());
+        assertEquals(0, testShow.getWatchTime());
+        assertEquals(0, testShow.getEpisode());
+        assertEquals(0, testShow.getSeason());
+    }
+
+    @Test
+    public void testSetSeasonOnce() {
+        testShow.setSeason(1);
+        assertEquals(1, testShow.getSeason());
+    }
+
+    @Test
+    public void testSetSeasonMultipleTimes() {
+        testShow.setSeason(2);
+        assertEquals(2, testShow.getSeason());
+        testShow.setSeason(3);
+        assertEquals(3, testShow.getSeason());
+    }
+
+    @Test
+    public void testSetEpisodeOnce() {
+        testShow.setEpisode(1);
+        assertEquals(1, testShow.getEpisode());
+    }
+
+    @Test
+    public void testSetEpisodeMultipleTime() {
+        testShow.setEpisode(2);
+        assertEquals(2, testShow.getEpisode());
+        testShow.setEpisode(3);
+        assertEquals(3, testShow.getEpisode());
+    }
+
+    @Test
+    public void testEqualsSameFields() {
+        assertTrue(testShow.equals(new Show("Watched", "test", "genre")));
+    }
+
+    @Test
+    public void testEqualsSameType() {
+        assertTrue(testShow.equals(new Show("To-watch", "Spiderman", "Action")));
+    }
+
+    @Test
+    public void testEqualsNotSameType() {
+        assertFalse(testShow.equals(new Movie("Watched", "test", "genre")));
+    }
+}
