f61a995c1d662429dc5b0bb1496b8d0b8f274086
Derek Zhao <dz33@students.cs.ubc.ca>
2025-02-28 22:54:31 -0800

    added JsonReader class

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,17 @@
+package persistence;
+
+import model.Movie;
+import model.MovieList;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.json.*;
+
+
+public class JsonReader {
+    
+}
