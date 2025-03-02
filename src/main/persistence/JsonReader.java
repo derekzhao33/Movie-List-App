50dd17c5e7ec938b431c502d9ecb68b222940aae
Derek Zhao <dz33@students.cs.ubc.ca>
2025-03-01 22:35:18 -0800

    added JsonReader method specifications

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -13,5 +13,38 @@ import org.json.*;
 
 
 public class JsonReader {
-    
+    private String source;
+
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+
+    }
+
+    // EFFECTS: reads movielist from file and returns it;
+    // throws IOException if an error occurs reading data from file
+    public MovieList read() throws IOException {
+        return new MovieList(); // stub
+    }
+
+    // EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        return ""; // stub
+     }
+
+    // EFFECTS: parses movielist from JSON object and returns it
+    private void parseMovieList(JSONObject jsonObject) {
+        // TODO
+    }
+
+    // MODIFIES: ml
+    // EFFECTS: parses movies from JSON object and adds them to movielist
+    private void addMovies(MovieList ml, JSONObject jsonObject) {
+        // TODO
+    }
+
+    // MODIFIES: ml
+    // EFFECTS: parses movie from JSON object and adds it to movielist
+    private void addMovie(MovieList ml, JSONObject jsonObject) {
+        // TODO
+    }
 }
