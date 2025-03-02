ffa1609a98e71ee528d603a3f9a8481637473cf0
Derek Zhao <dz33@students.cs.ubc.ca>
2025-03-02 03:49:23 -0800

    added JsonWriter and specifications

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,43 @@
+package persistence;
+import model.MovieList;
+import model.Movie;
+import org.json.JSONObject;
+
+import java.io.*;
+
+// Represents a writer that writes JSON representation of a movie list to file
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of movie list to file
+    public void write(MovieList ml) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+
+    }
+}
