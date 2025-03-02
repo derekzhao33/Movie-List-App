066e62c2f9126d705a109c6bce60659272de47f7
Derek Zhao <dz33@students.cs.ubc.ca>
2025-03-02 04:04:39 -0800

    fixed import statement

--- src/test/persistence/TestJsonReader.java
+++ src/test/persistence/TestJsonReader.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.*;
 
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestJsonReader {
     
