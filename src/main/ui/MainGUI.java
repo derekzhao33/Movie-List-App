c8c70c63672fadc55da85c3709310f3710ef33cf
Derek Zhao <dz33@students.cs.ubc.ca>
2025-03-21 00:07:39 -0700

    added class level documentation for main

--- src/main/ui/MainGUI.java
+++ src/main/ui/MainGUI.java
@@ -1,9 +1,15 @@
 package ui;
 
+import java.awt.Color;
 import java.io.FileNotFoundException;
+import javax.swing.*;
 
+import model.*;
+
+// Runs the GUI
 public class MainGUI {
 
+    // EFFECTS: runs the GUI
     public static void main(String[] args) {
         
     }
