b753d82070738656ae8660a4da4fb36dd257f22c
Derek Zhao <dz33@students.cs.ubc.ca>
2025-03-24 16:12:29 -0700

    added logoLabel to StartPanel

--- src/main/ui/graphics/panels/StartPanel.java
+++ src/main/ui/graphics/panels/StartPanel.java
@@ -5,6 +5,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// Represents a panel for the start screen
 public class StartPanel extends JPanel {
+    private static final String LOGO_PATH = "data/logo.png";
+    private ImageIcon image;
+    private JLabel logoLabel;
     
+    //EFFECTS: creates a new StartPanel
+    public StartPanel() {
+        this.image = new ImageIcon(LOGO_PATH);
+        this.image = new ImageIcon(image.getImage().getScaledInstance(150, 130, Image.SCALE_SMOOTH));
+        this.logoLabel = new JLabel("Movie List Application");
+        this.logoLabel.setIcon(image);
+        this.logoLabel.setHorizontalTextPosition(JLabel.CENTER);
+        this.logoLabel.setVerticalTextPosition(JLabel.BOTTOM);
+        add(logoLabel);
+    }
 }
