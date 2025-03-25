8cee1a82276d6c617ebe7f10d04799888627b7f0
Derek Zhao <dz33@students.cs.ubc.ca>
2025-03-25 14:27:18 -0700

    fixed merge conflicts

--- src/main/ui/graphics/MovieFrame.java
+++ src/main/ui/graphics/MovieFrame.java
@@ -1,17 +1,16 @@
 package ui.graphics;
 
-import model.Movie;
 import model.MovieList;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 
-import model.*;
-import persistence.*;
 import ui.graphics.panels.CardPanel;
 
 // Represents a frame for the movie list application
@@ -20,23 +19,18 @@ public class MovieFrame extends JFrame implements ActionListener {
     private static final String LOGO_PATH = "data/logo.png";
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
-    private MenuHandler menuHandler;
     private CardPanel cardPanel;
     private MovieList movieList;
     private JTextArea textArea;
     private MenuHandler menuHandler;
-    private JLabel label;
 
     // EFFECTS: creates a new frame
     public MovieFrame() {
-        this.image = new ImageIcon("data/logo.png");
-        this.image = new ImageIcon(image.getImage().getScaledInstance(500, 500, 1));
         this.movieList = new MovieList();
         this.jsonWriter = new JsonWriter(JSON_STORE);
         this.jsonReader = new JsonReader(JSON_STORE);
         this.movieList = new MovieList();
         this.menuHandler = new MenuHandler(this);
-        this.label = new JLabel(image);
 
         initializeFrame();
 
@@ -62,9 +56,9 @@ public class MovieFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == menuHandler.getLoadMenuItem()) {
-            loadMovies();
+            loadMovieList();
         } else if (e.getSource() == menuHandler.getSaveMenuItem()) {
-            saveMovies();
+            saveMovieList();
         } else if (e.getSource() == menuHandler.getAddMenuItem()) {
             this.cardPanel.switchPanels("add", this.movieList);
         } else if (e.getSource() == menuHandler.getRemoveMenuItem()) {
@@ -99,38 +93,5 @@ public class MovieFrame extends JFrame implements ActionListener {
         } catch (FileNotFoundException e) {
             textArea.setText("Unable to write to file: " + JSON_STORE);
         }
-    }
-
-    // MODIFIES: this
-    // EFFECTS: adds a new movie to the list
-    private void addMovie() {
-        String status = JOptionPane.showInputDialog("Enter the status of the new movie (w, c, t):");
-        String name = JOptionPane.showInputDialog("Enter the name of the new movie:");
-        String genre = JOptionPane.showInputDialog("Enter the genre of the new movie:");
-
-        if (status != null && name != null && genre != null) {
-            movieList.addMovie(new Movie(status, name, genre));
-            textArea.setText("Added movie: " + name);
-        } else {
-            textArea.setText("Movie addition cancelled.");
-        }
-    }
-
-    // MODIFIES: this
-    // EFFECTS: removes a movie from the list
-    private void removeMovie() {
-        String input = JOptionPane.showInputDialog("Enter the number of the movie to be removed:");
-        if (input != null) {
-            int movieNum = Integer.parseInt(input);
-            movieList.removeMovie(movieNum);
-            textArea.setText("Removed movie number: " + movieNum);
-        } else {
-            textArea.setText("Movie removal cancelled.");
-        }
-    }
-
-    // EFFECTS: displays all movies in the list
-    private void displayMovies() {
-        textArea.setText(movieList.getAllNames());
     }
 }
